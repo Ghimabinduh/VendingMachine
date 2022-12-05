ce47a55ca19a6ec920fbbaf5947687e7597cf88a
Ghimabinduh <119585005+Ghimabinduh@users.noreply.github.com>
2022-12-05 15:23:57 +0530

    Add files via upload
    I have added unit test cases files please check them.

---
+++ UnitTest.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachine.ViewModel;
+
+namespace VendingMachineUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        VendingMachineViewModel viewmodel = new VendingMachineViewModel();
+        [TestMethod]
+        public async void ValidateDataTestMethod()
+        {
+         await   viewmodel.ValidateData(Coins.Dimes.ToString(), 0, Products.candy.ToString());
+            Assert.IsNotNull(viewmodel.Confirmationtext);
+                
+        }
+        [TestMethod]
+        public  void SetProcessVisibilityTestMethod()
+        {
+             viewmodel.SetProcessVisibility(Coins.Dimes.ToString(),0,Products.chips.ToString());
+            Assert.AreEqual(viewmodel.ProcessVisibility,Windows.UI.Xaml.Visibility.Visible);
+
+        }
+    }
+}
